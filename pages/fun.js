8ad8d882b252654e99c67fe9968e91eb8224a28d
Gabriel Cornejo <gcornejo441@gmail.com>
2023-03-15 09:06:22 -0700

    updates to ascii art

--- pages/fun.js
+++ pages/fun.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import figlet from 'figlet'
 import standard from 'figlet/importable-fonts/Standard.js'
+import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
 
 figlet.parseFont('Standard', standard)
 
@@ -9,7 +11,7 @@ function AsciiArt() {
   const [asciiArt, setAsciiArt] = useState('')
 
   const handleNameChange = (event) => {
-    setName(event.target.value)
+    setName(event.target.value.slice(0, 50)) // set the max limit to 50 characters
   }
 
   const handleSubmit = (event) => {
@@ -31,28 +33,32 @@ function AsciiArt() {
   }
 
   return (
-    <div className="bg-gray-100 py-10">
+    <div className="py-10">
       <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
         <div className="mb-4 rounded-lg bg-white px-8 pt-6 pb-8 shadow-lg">
           <h2 className="mb-4 text-xl font-bold">ASCII Art Generator</h2>
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
-              <label className="mb-2 block font-bold text-gray-700">Enter your name:</label>
-              <input
-                className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              <label className="mb-2 block font-bold text-gray-700">Make some art:</label>
+              <textarea
+                required
+                className="focus:shadow-outline w-full resize-none appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
                 type="text"
                 value={name}
                 onChange={handleNameChange}
+                maxLength="50"
+                rows="1"
               />
+              <span className="text-sm text-gray-500">{name.length}/50</span>
             </div>
             <button
               type="submit"
-              className="focus:shadow-outline rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
+              className="inline-block rounded-lg bg-forestGreen600 px-4 py-3 font-medium text-white hover:bg-desertSand hover:text-forestGreen600"
             >
-              Generate ASCII art
+              Generate
             </button>
           </form>
-          {asciiArt && <pre className="mt-6 overflow-x-auto whitespace-pre-wrap">{asciiArt}</pre>}
+          {asciiArt && <pre className="mt-6 overflow-x-auto">{asciiArt}</pre>}
         </div>
       </div>
     </div>
@@ -60,5 +66,10 @@ function AsciiArt() {
 }
 
 export default function Home() {
-  return <AsciiArt />
+  return (
+    <>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <AsciiArt />
+    </>
+  )
 }
