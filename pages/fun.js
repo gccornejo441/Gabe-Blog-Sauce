82c99386af992f7e02cdcbdfa262adb444f9ada4
Gabriel Cornejo <gcornejo441@gmail.com>
2023-03-15 13:05:30 -0700

    adding horizontal layout feature to ascii art

--- pages/fun.js
+++ pages/fun.js
@@ -1,25 +1,46 @@
 import { useState } from 'react'
 import figlet from 'figlet'
-import standard from 'figlet/importable-fonts/Standard.js'
 import { PageSEO } from '@/components/SEO'
+import standard from 'figlet/importable-fonts/Standard.js'
+import banner from 'figlet/importable-fonts/Banner.js'
+import small from 'figlet/importable-fonts/Small.js'
 import siteMetadata from '@/data/siteMetadata'
+import ansiShadow from 'figlet/importable-fonts/ANSI Shadow.js'
 
-figlet.parseFont('Standard', standard)
+const availableFonts = [
+  { name: 'Standard', font: standard },
+  { name: 'Big', font: banner },
+  { name: 'Small', font: small },
+  { name: 'ANSI Shadow', font: ansiShadow },
+  // Add more fonts here
+]
 
 function AsciiArt() {
   const [name, setName] = useState('')
+  const [font, setFont] = useState('Standard')
   const [asciiArt, setAsciiArt] = useState('')
+  const [horizontalLayout, setHorizontalLayout] = useState('default')
 
   const handleNameChange = (event) => {
     setName(event.target.value.slice(0, 50)) // set the max limit to 50 characters
   }
 
+  const handleFontChange = (event) => {
+    setFont(event.target.value)
+  }
+
+  const handleHorizontalLayoutChange = (event) => {
+    setHorizontalLayout(event.target.value)
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
+    figlet.parseFont(font, availableFonts.find((f) => f.name === font).font)
     figlet.text(
       name,
       {
-        font: 'Standard',
+        font: font,
+        horizontalLayout: horizontalLayout,
       },
       (err, data) => {
         if (err) {
@@ -50,6 +71,34 @@ function AsciiArt() {
                 rows="1"
               />
               <span className="text-sm text-gray-500">{name.length}/50</span>
+            </div>
+            <div className="mb-4">
+              <label className="mb-2 block font-bold text-gray-700">Select font:</label>
+              <select
+                className="focus:shadow-outline min-w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+                value={font}
+                onChange={handleFontChange}
+              >
+                {availableFonts.map((f) => (
+                  <option key={f.name} value={f.name}>
+                    {f.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="mb-4">
+              <label className="mb-2 block font-bold text-gray-700">Horizontal Layout:</label>
+              <select
+                className="focus:shadow-outline min-w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+                value={horizontalLayout}
+                onChange={handleHorizontalLayoutChange}
+              >
+                <option value="default">Default</option>
+                <option value="full">Full</option>
+                <option value="fitted">Fitted</option>
+                <option value="controlled smushing">Controlled Smushing</option>
+                <option value="universal smushing">Universal Smushing</option>
+              </select>
             </div>
             <button
               type="submit"
