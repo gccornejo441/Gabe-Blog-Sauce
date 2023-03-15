38d467c4667ae494f9b143186ecba9dd0ca8eed7
Gabriel Cornejo <gcornejo441@gmail.com>
2023-03-15 13:29:10 -0700

    updating darkmode on /fun

--- pages/fun.js
+++ pages/fun.js
@@ -41,6 +41,8 @@ function AsciiArt() {
       {
         font: font,
         horizontalLayout: horizontalLayout,
+        width: 80,
+        whitespaceBreak: true,
       },
       (err, data) => {
         if (err) {
@@ -56,26 +58,32 @@ function AsciiArt() {
   return (
     <div className="py-10">
       <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
-        <div className="mb-4 rounded-lg bg-white px-8 pt-6 pb-8 shadow-lg">
-          <h2 className="mb-4 text-xl font-bold">ASCII Art Generator</h2>
+        <div className="mb-4 rounded-lg bg-forestGreen600 px-8 pt-6 pb-8 text-gray-100 shadow-lg dark:bg-desertSand dark:text-gray-900">
+          <h2 className="mb-4 text-xl font-bold dark:divide-gray-700">ASCII Art Generator</h2>
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
-              <label className="mb-2 block font-bold text-gray-700">Make some art:</label>
+              <label className="mb-2 block font-bold text-sageGreen dark:text-forestGreen600">
+                Make some art:
+              </label>
               <textarea
                 required
-                className="focus:shadow-outline w-full resize-none appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+                className="focus:shadow-outline w-full resize-none appearance-none rounded border py-2 px-3 leading-tight text-sageGreen shadow focus:outline-none dark:text-forestGreen600"
                 type="text"
                 value={name}
                 onChange={handleNameChange}
                 maxLength="50"
                 rows="1"
               />
-              <span className="text-sm text-gray-500">{name.length}/50</span>
+              <span className="text-sm text-gray-500 text-sageGreen dark:text-forestGreen600">
+                {name.length}/50
+              </span>
             </div>
             <div className="mb-4">
-              <label className="mb-2 block font-bold text-gray-700">Select font:</label>
+              <label className="mb-2 block font-bold text-sageGreen dark:text-forestGreen600">
+                Select font:
+              </label>
               <select
-                className="focus:shadow-outline min-w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+                className="focus:shadow-outline min-w-full appearance-none rounded border py-2 px-3 leading-tight text-sageGreen shadow focus:outline-none dark:text-forestGreen600"
                 value={font}
                 onChange={handleFontChange}
               >
@@ -87,9 +95,11 @@ function AsciiArt() {
               </select>
             </div>
             <div className="mb-4">
-              <label className="mb-2 block font-bold text-gray-700">Horizontal Layout:</label>
+              <label className="mb-2 block font-bold text-sageGreen dark:text-forestGreen600">
+                Horizontal Layout:
+              </label>
               <select
-                className="focus:shadow-outline min-w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+                className="focus:shadow-outline min-w-full appearance-none rounded border py-2 px-3 leading-tight text-sageGreen shadow focus:outline-none dark:text-forestGreen600"
                 value={horizontalLayout}
                 onChange={handleHorizontalLayoutChange}
               >
@@ -102,12 +112,12 @@ function AsciiArt() {
             </div>
             <button
               type="submit"
-              className="inline-block rounded-lg bg-forestGreen600 px-4 py-3 font-medium text-white hover:bg-desertSand hover:text-forestGreen600"
+              className="inline-block rounded-lg bg-desertSand px-4 py-3 font-medium text-forestGreen600 hover:bg-sageGreen hover:text-forestGreen600 dark:bg-forestGreen600 dark:text-desertSand"
             >
               Generate
             </button>
           </form>
-          {asciiArt && <pre className="mt-6 overflow-x-auto">{asciiArt}</pre>}
+          {asciiArt && <pre className="mt-6 overflow-x-auto whitespace-pre-wrap">{asciiArt}</pre>}
         </div>
       </div>
     </div>
